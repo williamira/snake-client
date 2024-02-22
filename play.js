6e0158ccf18bec683fdfc2e51c14cbd88db2abf0
williamira <williamira22@gmail.com>
2024-02-21 20:48:51 -0700

    added function to handle input from use

--- play.js
+++ play.js
@@ -1,9 +1,25 @@
 const {connect} = require('./client')
 
-
-
 connect()
 
 
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
+
+const handleUserInput = function (data) {
+  if (data === '\u0003') { // if users presses ctrl c the game will end
+    process.exit()
+  }
+};
+
+setupInput();
 
 console.log("Connecting ...");
